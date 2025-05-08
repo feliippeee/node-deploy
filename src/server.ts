20186d902f5d2e39150fda950f79b43f379514f7
Felipe Marques <felipe.tecnosuporte@gmail.com>
2025-05-08 10:52:01 -0300

    new configuration prisma generate

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "./generated/prisma/client"
+import { PrismaClient } from "../prisma/node_modules/.prisma/client"
 import fastify from "fastify"
 import { z } from "zod"
 
